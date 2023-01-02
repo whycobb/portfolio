913e93e6ffb2cda1950ebad262565bc7cca0f026
YorickCobb <52218417+YorickCobb@users.noreply.github.com>
2023-01-02 11:39:15 -0500

    Update wakeup.py

--- wakeup.py
+++ wakeup.py
@@ -8,18 +8,6 @@ import re
 import sys
 
 
-def getRecords(domain): #grab all the records so we know which ones to delete to make room for our record. Also checks to make sure we've got the right domain
-	allRecords=json.loads(requests.post(apiConfig["endpoint"] + '/dns/retrieve/' + domain, data = json.dumps(apiConfig)).text)
-	if allRecords["status"]=="ERROR":
-		print('Error getting domain. Check to make sure you specified the correct domain, and that API access has been switched on for this domain.');
-		sys.exit();
-	return(allRecords)
-
-def getMyIP():
-	ping = json.loads(requests.post(apiConfig["endpoint"] + '/ping/', data = json.dumps(apiConfig)).text)
-	return(ping["yourIp"])
-
-
 
 
 #DNS Update function
