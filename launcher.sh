da4dca11320d9377e84a80ed5a8acf1d08b07d61
YorickCobb <52218417+YorickCobb@users.noreply.github.com>
2023-01-02 11:37:22 -0500

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,8 @@
 #launches python
 PYTHONPATH=/home/pi/.local/lib/python3.7/site-packages python3 /home/pi/portfolio/wakeup.py
 
+PYTHONPATH=/home/pi/.local/lib/python3.7/site-packages python3 /path/to/config.json yorickc.com
+
 echo 'HIYA'
 
 read -n 1 -s
