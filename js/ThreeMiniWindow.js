ead621803e397696143f1bcca2560f5c9c4b73ee
YorickCobb <ycobb1@umbc.edu>
2023-02-22 17:03:31 -0500

    blahabalel

--- js/ThreeMiniWindow.js
+++ js/ThreeMiniWindow.js
@@ -6,7 +6,9 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+
+myWindow = document.getElementById("window1");
+myWindow.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
