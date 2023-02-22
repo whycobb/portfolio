1f410c3e4d5134ed74418ee1431e664c4bcf9b6b
YorickCobb <ycobb1@umbc.edu>
2023-02-22 16:36:16 -0500

    Update ThreeMiniWindow.js

--- js/ThreeMiniWindow.js
+++ js/ThreeMiniWindow.js
@@ -8,5 +8,12 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
 console.log("ThreeMiniWindow is running!");
 
