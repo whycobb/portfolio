283708fa4f9e2e5814e2d42d557675ff69b25ac9
YorickCobb <ycobb1@umbc.edu>
2023-02-23 15:14:59 -0500

    ugh

--- js/ThreeMiniWindow.js
+++ js/ThreeMiniWindow.js
@@ -13,7 +13,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( width, 350 );
+renderer.setSize( width, height );
 
 myWindow.appendChild( renderer.domElement );
 
