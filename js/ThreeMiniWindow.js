0fba112eda23ef6cc349854d5ec77c026cafbab7
YorickCobb <ycobb1@umbc.edu>
2023-02-23 15:05:20 -0500

    Update ThreeMiniWindow.js

--- js/ThreeMiniWindow.js
+++ js/ThreeMiniWindow.js
@@ -13,7 +13,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( width, 50 );
+renderer.setSize( width, 350 );
 
 myWindow.appendChild( renderer.domElement );
 
