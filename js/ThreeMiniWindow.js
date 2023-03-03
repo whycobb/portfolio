a94a8b2b7a599b9d0ea3b346cf124120f10e32da
YorickCobb <ycobb1@umbc.edu>
2023-03-03 16:55:13 -0500

    Update ThreeMiniWindow.js

--- js/ThreeMiniWindow.js
+++ js/ThreeMiniWindow.js
@@ -50,7 +50,7 @@ objLoader.load(		//load cottage geometry
 		brainGeo.scale.set( 1, 1, 1 );
 		brainGeo.position.set( 0, 0, 0 );
 		brainGeo.rotation.order = "YZX";
-		brainGeo.rotation.set( 0/57.2957795131, 0/57.2957795131, 90/57.2957795131 );
+		brainGeo.rotation.set( 0/57.2957795131, 0/57.2957795131, 0/57.2957795131 );
 	}
 );
 
