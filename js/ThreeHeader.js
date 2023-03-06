3b3674d72a20d7c98fb56586cc977068a7593ade
YorickCobb <ycobb1@umbc.edu>
2023-03-06 09:06:46 -0500

    Update ThreeHeader.js

--- js/ThreeHeader.js
+++ js/ThreeHeader.js
@@ -39,7 +39,7 @@ var brainGeo;
 //load brain
 var objLoader = new OBJLoader();
 objLoader.load(		//load cottage geometry
-	"./Assets/Header/Geo_Thin.obj",
+	"./Assets/Header/Geo_Flat.obj",
 	function(object) {
 		brainGeo = object.children[0];
 		brainGeo.material = bwMaterial;
