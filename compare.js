a78e44a9227fa43c5b545bab57acb1aa0f0e0106
YorickCobb <ycobb1@umbc.edu>
2023-04-26 13:12:28 -0400

    Compare updates

--- compare.js
+++ compare.js
@@ -1,40 +1,23 @@
-
-console.log("Hi!");
-
 var overlays = [];
 var overlaid = false;
 
 function togglewf() {
-	console.log("Hi again!");
-	
 	if(overlays.length ==0) {
-		console.log("No overlays found yet");
 		overlays = document.getElementsByClassName("compare-overlay");
 	}
 	if(overlays.length > 0) {
-		console.log("Overlays found!");
-		
 		if(overlaid) {
-			
 			for(let i = 0; i < overlays.length; i++) {
-				console.log("Overlay..." + (i+1));
 				overlays[i].style.opacity = "0%";
+				overlays[i].style.pointerEvents = "none";
 			}
 			overlaid = false;
-			
 		} else {
-			
 			for(let i = 0; i < overlays.length; i++) {
-				console.log("Overlay..." + (i+1));
 				overlays[i].style.opacity = "100%";
+				overlays[i].style.pointerEvents = "auto";
 			}
 			overlaid = true;
-			
 		}
-		
-		
-		
-		
-		
 	}
 }
