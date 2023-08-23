befe4e7d9a1853cef017c2ef8e6f8dd5f101148a
YorickCobb <ycobb1@umbc.edu>
2023-08-23 15:34:48 -0400

    Update ws.js

--- ws.js
+++ ws.js
@@ -1,12 +1,23 @@
 var http = require('http'),
+		fs = require('fs'),
 		dt = require('./ycDTModule.js'),
 		port = process.argv[2] || 1178;
 
+
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
+	fs.readFile('index.html', function(err, data) {
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write(data);
+		return response.end();
+	});
 	
-  response.write("Current date + time: " + dt.getDateTime() + "; URL was '" + request.url + "'");
-	response.end();
+	
+	
+	
+//  response.writeHead(200, {'Content-Type': 'text/html'});
+	
+//  response.write("Current date + time: " + dt.getDateTime() + "; URL was '" + request.url + "'");
+//	response.end();
 }).listen(parseInt(port, 10)); 
 
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
