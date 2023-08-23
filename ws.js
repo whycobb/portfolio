8330ec7a285c0516d2c71a154fa2c73e96418044
YorickCobb <ycobb1@umbc.edu>
2023-08-23 12:12:53 -0400

    Update ws.js

--- ws.js
+++ ws.js
@@ -3,8 +3,8 @@ var http = require('http'),
 		port = process.argv[2] || 1178;
 
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write("Current date + time: " + dt.getDateTime());
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write("Current date + time: " + dt.getDateTime() + "; URL was '" + request.url + "'");
 	response.end();
 }).listen(parseInt(port, 10)); 
 
