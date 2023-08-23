dc550e32c756c6c7f42ad50fbc4aa93d55a00129
YorickCobb <ycobb1@umbc.edu>
2023-08-23 12:04:39 -0400

    Some Node exercises

--- ws.js
+++ ws.js
@@ -1,9 +1,11 @@
 var http = require('http'),
+		dt = require('./ycDTModule.js'),
 		port = process.argv[2] || 1178;
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write("Current date + time: " + dt.getDateTime());
+	response.end();
 }).listen(parseInt(port, 10)); 
 
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
---
+++ ycDTModule.js
@@ -0,0 +1,3 @@
+exports.getDateTime = function() {
+	return Date();
+};
