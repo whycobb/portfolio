2cca5043baf828fe0db0cfbadb08470dd0dc3be5
YorickCobb <ycobb1@umbc.edu>
2023-08-23 11:27:05 -0400

    Update ws.js

--- ws.js
+++ ws.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
-http.createServer(function (request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello World!');
-	
-}).listen(parseInt(port, 10));
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(parseInt(port, 10)); 
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
